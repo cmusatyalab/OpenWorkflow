400c27771829993526dab1de5cf825982abd0318
Junjue Wang <junjuew@cs.cmu.edu>
2018-12-23 23:35:42 -0500

    add in working gen-jspb.sh

--- gabrieltool/statemachine-editor-react/gen-jspb.sh
+++ gabrieltool/statemachine-editor-react/gen-jspb.sh
@@ -3,4 +3,4 @@
 # from .proto files.
 
 protoc --proto_path=../statemachine/proto --js_out=import_style=commonjs,binary:src ../statemachine/proto/wca-state-machine.proto && \
-sed -i '1s/^/<added text> /\n' src/wca-state-machine_pb.js
+sed -i '1s/^/\/* eslint-disable *\/\n/' src/wca-state-machine_pb.js
