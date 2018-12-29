5643bb4335677af2ac260d0f2c4218cc77021d27
Junjue Wang <junjuew@cs.cmu.edu>
2018-12-29 01:06:13 -0500

    add display for transition info

--- gabrieltool/statemachine-editor-react/src/infoBox.js
+++ gabrieltool/statemachine-editor-react/src/infoBox.js
@@ -3,84 +3,85 @@ import ReactTable from "react-table";
 import matchSorter from "match-sorter";
 import ListGroup from "react-bootstrap/lib/ListGroup";
 import ListGroupItem from "react-bootstrap/lib/ListGroupItem";
+import { FSMElementType } from "./toolbar.js";
 import "react-table/react-table.css";
 
 const InfoBox = ({ element }) => {
   console.log("infobox render called");
   const elementType =
-    typeof element.getProcessorsList === "function" ? "State" : "Transition";
+    typeof element.getProcessorsList === "function" ? FSMElementType.STATE : FSMElementType.TRANSITION;
   const tableData =
-    elementType === "State"
+    elementType === FSMElementType.STATE
       ? element.getProcessorsList().map(callableItem => {
-          return {
-            name: callableItem.getName(),
-            callable_name: callableItem.getCallableName(),
-            callable_args: callableItem.getCallableArgs()
-          };
-        })
+        return {
+          name: callableItem.getName(),
+          callable_name: callableItem.getCallableName(),
+          callable_args: callableItem.getCallableArgs()
+        };
+      })
       : element.getPredicatesList().map(callableItem => {
-          return {
-            name: callableItem.getName(),
-            callable_name: callableItem.getCallableName(),
-            callable_args: callableItem.getCallableArgs()
-          };
-        });
+        return {
+          name: callableItem.getName(),
+          callable_name: callableItem.getCallableName(),
+          callable_args: callableItem.getCallableArgs()
+        };
+      });
   const tableColumns =
-    elementType === "State"
+    elementType === FSMElementType.STATE
       ? [
-          {
-            Header: "Processor Name",
-            accessor: "name",
-            filterMethod: (filter, rows) =>
-              matchSorter(rows, filter.value, { keys: ["name"] }),
-            filterAll: true
-          },
-          {
-            Header: "Processing Function",
-            accessor: "callable_name",
-            filterMethod: (filter, rows) =>
-              matchSorter(rows, filter.value, {
-                keys: ["callable_name"]
-              }),
-            filterAll: true
-          },
-          {
-            Header: "Arguments",
-            accessor: "callable_args",
-            filterMethod: (filter, rows) =>
-              matchSorter(rows, filter.value, {
-                keys: ["callable_args"]
-              }),
-            filterAll: true
-          }
-        ]
+        {
+          Header: "Processor Name",
+          accessor: "name",
+          filterMethod: (filter, rows) =>
+            matchSorter(rows, filter.value, { keys: ["name"] }),
+          filterAll: true
+        },
+        {
+          Header: "Processing Function",
+          accessor: "callable_name",
+          filterMethod: (filter, rows) =>
+            matchSorter(rows, filter.value, {
+              keys: ["callable_name"]
+            }),
+          filterAll: true
+        },
+        {
+          Header: "Arguments",
+          accessor: "callable_args",
+          filterMethod: (filter, rows) =>
+            matchSorter(rows, filter.value, {
+              keys: ["callable_args"]
+            }),
+          filterAll: true
+        }
+      ]
       : [
-          {
-            Header: "Predicate Name",
-            accessor: "name",
-            filterMethod: (filter, rows) =>
-              matchSorter(rows, filter.value, { keys: ["name"] }),
-            filterAll: true
-          },
-          {
-            Header: "Predicate Function",
-            accessor: "callable_name",
-            filterMethod: (filter, rows) =>
-              matchSorter(rows, filter.value, {
-                keys: ["callable_name"]
-              }),
-            filterAll: true
-          },
-          {
-            Header: "Arguments",
-            accessor: "callable_args",
-            filterMethod: (filter, rows) =>
-              matchSorter(rows, filter.value, {
-                keys: ["callable_args"]
-              }),
-            filterAll: true
-          }
-        ];
+        {
+          Header: "Predicate Name",
+          accessor: "name",
+          filterMethod: (filter, rows) =>
+            matchSorter(rows, filter.value, { keys: ["name"] }),
+          filterAll: true
+        },
+        {
+          Header: "Predicate Function",
+          accessor: "callable_name",
+          filterMethod: (filter, rows) =>
+            matchSorter(rows, filter.value, {
+              keys: ["callable_name"]
+            }),
+          filterAll: true
+        },
+        {
+          Header: "Arguments",
+          accessor: "callable_args",
+          filterMethod: (filter, rows) =>
+            matchSorter(rows, filter.value, {
+              keys: ["callable_args"]
+            }),
+          filterAll: true
+        }
+      ];
   return (
     <div>
       <h3>Information</h3>
@@ -91,6 +92,15 @@ const InfoBox = ({ element }) => {
         <ListGroupItem>
           <h4>Type: {elementType}</h4>
         </ListGroupItem>
+        {
+          elementType === FSMElementType.TRANSITION &&
+          <ListGroupItem>
+            <h4>Instruction</h4>
+            <h5>Audio:</h5><p>{element.getInstruction().getAudio()}</p>
+            <h5>Image:</h5><p>{element.getInstruction().getImage()}</p>
+            <h5>Video:</h5><p>{element.getInstruction().getVideo()}</p>
+          </ListGroupItem>
+        }
         <ListGroupItem>
           {(
             <ReactTable
