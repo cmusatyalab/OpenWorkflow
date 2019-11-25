db2092d8b98bc9db3fee754a2900858988f8ec03
junjuew <junjuew@cs.cmu.edu>
2019-11-24 23:44:08 -0500

    add in containerized faster-rcnn processor

--- gabrieltool/statemachine/processor_zoo/containerized.py
+++ gabrieltool/statemachine/processor_zoo/containerized.py
@@ -1,32 +1,71 @@
+from .base import SerializableProcessor, record_kwargs
 import inspect
 import json
 import os
+import io
+import time
 from functools import wraps
 
 import cv2
 import numpy as np
-from cv2 import dnn
 from logzero import logger
 import copy
+import requests
 
-from .base import SerializableProcessor, record_kwargs
+import docker
+docker_client = docker.from_env()
 
 
 class FasterRCNNContainerProcessor(SerializableProcessor):
+    # UNIQUE to each python process
+    CONTAINER_NAME = 'FasterRCNNContainerProcessor-{}'.format(os.getpid())
+    CONTAINER_MAPPED_HOST_PORT = -1
 
     @record_kwargs
-    def __init__(self, container_image_url, labels=None, conf_threshold=0.8):
+    def __init__(self, container_image_url, labels=None, conf_threshold=0.5):
         # For default parameter settings,
         # see:
         # https://github.com/rbgirshick/fast-rcnn/blob/b612190f279da3c11dd8b1396dd5e72779f8e463/lib/fast_rcnn/config.py
         super(FasterRCNNContainerProcessor, self).__init__()
+        self.container_image_url = container_image_url
+        self.conf_threshold = conf_threshold
+        # port number inside the container that is open
+        self.container_port = '8000/tcp'
+        self.container = self._get_container()
+        if self.container is None or self.container.status != 'running':
+            self.container = self._start_container()
+
+    @property
+    def container_server_url(self):
+        return 'http://localhost:{}/detect'.format(self.CONTAINER_MAPPED_HOST_PORT)
+
+    def _start_container(self):
+        container = docker_client.containers.run(
+            self.container_image_url,
+            command='/bin/bash run_server.sh',
+            name=self.CONTAINER_NAME,
+            auto_remove=True,
+            detach=True,
+            ports={self.container_port: None}  # map container 8000 to a random host port
+        )
+        container.reload()
+        time.sleep(10)
+        self.CONTAINER_MAPPED_HOST_PORT = container.ports[self.container_port][0]['HostPort']
+        return container
+
+    def _get_container(self):
+        containers = docker_client.containers.list(filters={'name': self.CONTAINER_NAME})
+        if len(containers) > 0:
+            return containers[0]
+        return None
 
     @classmethod
     def from_json(cls, json_obj):
         try:
             kwargs = copy.copy(json_obj)
+            kwargs['container_image_url'] = json_obj['container_image_url']
             kwargs['labels'] = json_obj['labels']
-            kwargs['_conf_threshold'] = float(json_obj['conf_threshold'])
+            kwargs['conf_threshold'] = float(json_obj['conf_threshold'])
         except ValueError as e:
             raise ValueError(
                 'Failed to convert json object to {} instance. '
@@ -35,69 +74,31 @@ class FasterRCNNContainerProcessor(SerializableProcessor):
         return cls(**kwargs)
 
     def __call__(self, image):
-        height, width = image.shape[:2]
+        fp = io.BytesIO()
+        fp.write(cv2.imencode('.jpg', image)[1].tostring())
+        fp.seek(0, 0)
 
-        # resize image to correct size
-        im_size_min = np.min(image.shape[0:2])
-        im_size_max = np.max(image.shape[0:2])
-        im_scale = float(self._scale) / float(im_size_min)
-        # Prevent the biggest axis from being more than MAX_SIZE
-        if np.round(im_scale * im_size_max) > self._max_size:
-            im_scale = float(self._max_size) / float(im_size_max)
-        im = cv2.resize(image, None, None, fx=im_scale, fy=im_scale,
-                        interpolation=cv2.INTER_LINEAR)
-        # create input data
-        blob = cv2.dnn.blobFromImage(im, 1, (width, height), self._pixel_means,
-                                     swapRB=False, crop=False)
-        imInfo = np.array([height, width, im_scale], dtype=np.float32)
-        self._net.setInput(blob, 'data')
-        self._net.setInput(imInfo, 'im_info')
+        results = requests.post(self.container_server_url, data={
+            'confidence': self.conf_threshold,
+            'format': 'box'
+        }, files={
+            'picture': fp
+        })
+        return results.text
 
-        # infer
-        outs = self._net.forward(self._getOutputsNames(self._net))
-        t, _ = self._net.getPerfProfile()
-        logger.debug('Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency()))
+    def clean(self):
+        if self.container:
+            self.container.remove(force=True)
 
-        # postprocess
-        classIds = []
-        confidences = []
-        boxes = []
-        for out in outs:
-            for detection in out[0, 0]:
-                confidence = detection[2]
-                if confidence > self._conf_threshold:
-                    left = int(detection[3])
-                    top = int(detection[4])
-                    right = int(detection[5])
-                    bottom = int(detection[6])
-                    width = right - left + 1
-                    height = bottom - top + 1
-                    classIds.append(int(detection[1]) - 1)  # Skip background label
-                    confidences.append(float(confidence))
-                    boxes.append([left, top, width, height])
+# TODO: labels not used
 
-        indices = cv2.dnn.NMSBoxes(boxes, confidences, self._conf_threshold, self._nms_threshold)
-        results = {}
-        for i in indices:
-            i = i[0]
-            box = boxes[i]
-            left = box[0]
-            top = box[1]
-            width = box[2]
-            height = box[3]
-            classId = int(classIds[i])
-            confidence = confidences[i]
-            if self._labels[classId] not in results:
-                results[self._labels[classId]] = []
-            results[self._labels[classId]].append([left, top, left+width, top+height, confidence, classId])
 
-        if GABRIEL_DEBUG:
-            debug_image = image
-            for (class_name, detections) in list(results.items()):
-                for detection in detections:
-                    left, top, right, bottom, conf, _ = detection
-                    debug_image = drawPred(debug_image, class_name, conf, left, top, right, bottom)
-            cv2.imshow('debug', debug_image)
-            cv2.waitKey(1)
-        logger.debug('results: {}'.format(results))
-        return results
+if __name__ == "__main__":
+    container_image_url = 'registry.cmusatyalab.org/junjuew/container-registry:tpod-image-sandwich-sandwich'
+    label = 'bread'
+    processor = FasterRCNNContainerProcessor(container_image_url=container_image_url,
+                                             labels=[label],
+                                             conf_threshold=0.1)
+    image = cv2.imread('test.png')
+    print(processor(image))
+    processor.clean()
