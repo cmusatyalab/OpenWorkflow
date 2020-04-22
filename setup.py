a48e477b61a52aa1b5cc888cfb3263bd821e9f8c
junjuew <junjuew@cs.cmu.edu>
2020-04-22 13:34:19 -0400

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/junjuew/gabriel-tool',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
 )
