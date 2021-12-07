767c89c9d93e173752867c56e035812103260a00
Roger Iyengar <ri@rogeriyengar.com>
2021-12-06 20:14:19 -0500

    Updates

---
+++ precache-manifest.c39f7c857825a8045b3bdd979ab3347d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8948074268a040da635b",
+    "url": "/OpenWorkflow/static/css/main.a8b8abd6.chunk.css"
+  },
+  {
+    "revision": "8948074268a040da635b",
+    "url": "/OpenWorkflow/static/js/main.89480742.chunk.js"
+  },
+  {
+    "revision": "2eb8d8dba43bbff0e0e3",
+    "url": "/OpenWorkflow/static/css/1.195fcb7b.chunk.css"
+  },
+  {
+    "revision": "2eb8d8dba43bbff0e0e3",
+    "url": "/OpenWorkflow/static/js/1.2eb8d8db.chunk.js"
+  },
+  {
+    "revision": "8a2de10a721c2c70502c",
+    "url": "/OpenWorkflow/static/js/runtime~main.8a2de10a.js"
+  },
+  {
+    "revision": "97930d04abdee5568999dbd8a9e6bcf8",
+    "url": "/OpenWorkflow/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/OpenWorkflow/precache-manifest.44d6a00e1b3993522da410b2ed080c17.js"
+  "/OpenWorkflow/precache-manifest.c39f7c857825a8045b3bdd979ab3347d.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/OpenWorkflow/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
